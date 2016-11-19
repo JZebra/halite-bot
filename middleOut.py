96d8c35a975da637ba272ed756e2c1a3e616c6a6
John Shen <johnzhihaoshen@gmail.com>
2016-11-18 22:17:32 -0800

    first iteration middle_out

--- middleOut.py
+++ middleOut.py
@@ -3,17 +3,66 @@ import logging
 from hlt import *
 from networking import *
 
-_log = logging.getLogger(__name__)
+logging.basicConfig(filename='middleOut.log',level=logging.DEBUG)
+# _log = logging.getLogger(__name__)
 
 myID, gameMap = getInit()
+STR_CAP = 255
 sendInit("middle_out_00")
 
 
 def move(location):
     site = gameMap.getSite(location)
-    if site.strength < site.production * 5:
+
+    # capture neighbor if possible
+    capture_move = capture(location)
+    if capture_move:
+        logging.info('capturing move')
+        return capture_move
+
+    # farm if needed
+    if should_farm(location):
+        logging.info('farming move')
         return Move(location, STILL)
-    return Move(location, int((random.random() * 4) + 1))
+
+    # if no enemies and above critical size, move to neighbor with highest piece value
+    logging.info('reinforcing move')
+
+    return reinforce(location)
+
+def capture(location):
+    site = gameMap.getSite(location)
+    has_enemies = False
+    for direction in CARDINALS: 
+        neighbor = gameMap.getSite(location, direction)
+        is_friendly = neighbor.owner == site.owner
+        if neighbor.strength < site.strength and not is_friendly:
+            return Move(location, direction)
+    return None
+
+def reinforce(location):
+    site = gameMap.getSite(location)
+    # default values
+    strongest_neighbor = Site(strength=0)
+    target_direction = None
+
+    for direction in CARDINALS:
+        neighbor = gameMap.getSite(location, direction)
+        is_friendly = neighbor.owner == site.owner
+
+        if neighbor.strength > strongest_neighbor.strength and is_friendly and neighbor.strength + strongest_neighbor.strength < STR_CAP:
+            strongest_neighbor = neighbor
+            target_direction = direction
+
+    if target_direction:
+        return Move(location, target_direction)
+    else:
+        return Move(location, STILL)
+
+
+def should_farm(location):
+    site = gameMap.getSite(location)
+    return site.strength < site.production * 5
 
 
 while True:
